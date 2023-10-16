146b88bacacb21bab0a72df4321e23b2746dfb85
Carlos-Daniel-SS <carlossousa@rede.ulbra.br>
2023-10-16 18:11:01 -0300

    Atribuindo segurança ao banco de dados

--- src/main/java/br/com/carlosDaniel/todolist/user/UserContoller.java
+++ src/main/java/br/com/carlosDaniel/todolist/user/UserContoller.java
@@ -2,6 +2,12 @@ package br.com.carlosDaniel.todolist.user;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,8 +23,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/users")
 public class UserContoller {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getName());
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
+        }
+
+        var passwordhashred = BCrypt.withDefaults().hashToChar(12, userModel.getPassword().toCharArray());
+        String pass = passwordhashred.toString();
+        userModel.setPassword(pass);
+
+        var userCreate =  this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreate);
     }
 }
